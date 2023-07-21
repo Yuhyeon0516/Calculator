99e17bee825bf486b376367f1a4f65b1cea8ab1d
Yuhyeon0516 <rladbgus0516@naver.com>
2023-07-21 22:53:21 +0900

    I created calculator function and reset function

--- src/Calculator.js
+++ src/Calculator.js
@@ -23,36 +23,86 @@ const Calculator = () => {
   const [tempInput, setTempInput] = useState(null);
   const [tempOperator, setTempOperator] = useState(null);
 
+  const onPressNum = (num) => {
+    if (currentOperator) {
+      setResult(input);
+      setInput(num);
+    } else {
+      setInput(input * 10 + num);
+    }
+  };
+
+  const onPressOperator = (operator) => {
+    if (operator !== "=") {
+      setCurrentOperator(operator);
+    } else {
+      let finalResult = result;
+      switch (currentOperator) {
+        case "+":
+          finalResult = result + input;
+          break;
+        case "-":
+          finalResult = result - input;
+          break;
+        case "*":
+          finalResult = result * input;
+          break;
+        case "/":
+          finalResult = result / input;
+          break;
+
+        default:
+          break;
+      }
+      setResult(finalResult);
+      setInput(finalResult);
+    }
+  };
+
+  const onPressReset = () => {
+    setInput(0);
+    setCurrentOperator(null);
+    setResult(null);
+    setTempInput(null);
+    setTempOperator(null);
+  };
+
   return (
     <View style={{ flex: 1, width: 250, justifyContent: "center" }}>
+      <Text>input: {input}</Text>
+      <Text>currentOperator: {currentOperator}</Text>
+      <Text>result: {result}</Text>
+      <Text>tempInput: {tempInput}</Text>
+      <Text>tempOperator: {tempOperator}</Text>
+
       <InputContainer>
         <Text style={{ color: "white", fontSize: 35, textAlign: "right" }}>{input}</Text>
       </InputContainer>
       <ButtonContainer>
-        <Button type="reset" text="AC" onPress={() => {}} flex={3} />
-        <Button type="operator" text="/" onPress={() => {}} flex={1} />
+        <Button type="reset" text="AC" onPress={onPressReset} flex={3} />
+        <Button type="operator" text="/" onPress={() => onPressOperator("/")} flex={1} />
       </ButtonContainer>
       <ButtonContainer>
-        <Button type="num" text="7" onPress={() => {}} flex={1} />
-        <Button type="num" text="8" onPress={() => {}} flex={1} />
-        <Button type="num" text="9" onPress={() => {}} flex={1} />
-        <Button type="operator" text="X" onPress={() => {}} flex={1} />
+        <Button type="num" text="7" onPress={() => onPressNum(7)} flex={1} />
+        <Button type="num" text="8" onPress={() => onPressNum(8)} flex={1} />
+        <Button type="num" text="9" onPress={() => onPressNum(9)} flex={1} />
+        <Button type="operator" text="*" onPress={() => onPressOperator("*")} flex={1} />
       </ButtonContainer>
       <ButtonContainer>
-        <Button type="num" text="4" onPress={() => {}} flex={1} />
-        <Button type="num" text="5" onPress={() => {}} flex={1} />
-        <Button type="num" text="6" onPress={() => {}} flex={1} />
-        <Button type="operator" text="-" onPress={() => {}} flex={1} />
+        <Button type="num" text="4" onPress={() => onPressNum(4)} flex={1} />
+        <Button type="num" text="5" onPress={() => onPressNum(5)} flex={1} />
+        <Button type="num" text="6" onPress={() => onPressNum(6)} flex={1} />
+        <Button type="operator" text="-" onPress={() => onPressOperator("-")} flex={1} />
       </ButtonContainer>
       <ButtonContainer>
-        <Button type="num" text="1" onPress={() => {}} flex={1} />
-        <Button type="num" text="2" onPress={() => {}} flex={1} />
-        <Button type="num" text="3" onPress={() => {}} flex={1} />
-        <Button type="operator" text="+" onPress={() => {}} flex={1} />
+        <Button type="num" text="1" onPress={() => onPressNum(1)} flex={1} />
+        <Button type="num" text="2" onPress={() => onPressNum(2)} flex={1} />
+        <Button type="num" text="3" onPress={() => onPressNum(3)} flex={1} />
+        <Button type="operator" text="+" onPress={() => onPressOperator("+")} flex={1} />
       </ButtonContainer>
       <ButtonContainer>
-        <Button type="num" text="0" onPress={() => {}} flex={3} />
-        <Button type="operator" text="=" onPress={() => {}} flex={1} />
+        <Button type="num" text="0" onPress={() => onPressNum(0)} flex={3} />
+        <Button type="operator" text="=" onPress={() => onPressOperator("=")} flex={1} />
       </ButtonContainer>
     </View>
   );
